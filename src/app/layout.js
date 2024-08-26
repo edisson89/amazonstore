21b425424e6ba7c3d48b3cd40ef6d9c2f62557e4
edisson89 <edisson.12@hotmail.com>
2024-08-26 11:45:43 -0500

    agregando titulo metatags

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,13 +4,22 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Amazon",
+  title: "Amazon Ofertas",
   description: "Ofertas generadas en la Web",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+        <meta charset="UTF-8" />
+        <meta name="Descubre todas las ofertas que tenemos para ti en Amazon Prime,Electronica,Juguetes,Ropa.Ofertas y promociones de hoy " content="Ofertas generadas en la Web" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Amazon Ofertas" />
+        <meta property="og:description" content="Ofertas y promociones" />
+              
+        <title>Amazon Ofertas</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
