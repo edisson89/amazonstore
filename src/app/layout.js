869163295df2f31cc7a49c5c0af42c10dcef3776
edisson89 <edisson.12@hotmail.com>
2024-09-14 19:49:41 -0500

    ajustes formato

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,36 +1,59 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import imagen from './assets/logoamazon.png'
+/** @format */
 
-const inter = Inter({ subsets: ["latin"] });
+import { Inter } from "next/font/google"
+import "./globals.css"
+import imagen from "./assets/logoamazon.png"
+
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Amazon Ofertas",
-  description: "Ofertas generadas en la Web",
-
-};
+	title: "Amazon Ofertas",
+	description: "Ofertas generadas en la Web",
+}
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-        <head>
-        <meta charset="UTF-8" />
-        <link rel="icon" href="./favicon.ico" type="image/x-icon"></link>
-        <meta property="og:image" content={imagen} />
-        <meta name="Descubre todas las ofertas que tenemos para ti en Amazon Prime,Electronica,Juguetes,Ropa.Ofertas y promociones de hoy " content="Ofertas generadas en la Web" />
-        <meta name="robots" content="index, follow" />
-        <meta property="og:title" content="Amazon Ofertas" />
-        <meta property="og:description" content="Ofertas y promociones" />
-        
-              
-        <title>Amazon Ofertas</title>
-        
-      </head>
-      <body className={inter.className}>{children}</body>
-     
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NBVLCKCM"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	return (
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8" />
+				<link
+					rel="icon"
+					href="./favicon.ico"
+					type="image/x-icon"
+				></link>
+				<meta
+					property="og:image"
+					content={imagen}
+				/>
+				<meta
+					name="Descubre todas las ofertas que tenemos para ti en Amazon Prime,Electronica,Juguetes,Ropa.Ofertas y promociones de hoy "
+					content="Ofertas generadas en la Web"
+				/>
+				<meta
+					name="robots"
+					content="index, follow"
+				/>
+				<meta
+					property="og:title"
+					content="Amazon Ofertas"
+				/>
+				<meta
+					property="og:description"
+					content="Ofertas y promociones"
+				/>
 
-    </html>
-  );
+				<title>Amazon Ofertas</title>
+			</head>
+			<body className={inter.className}>{children}</body>
+
+			<noscript>
+				<iframe
+					src="https://www.googletagmanager.com/ns.html?id=GTM-NBVLCKCM"
+					height="0"
+					width="0"
+					style="display:none;visibility:hidden"
+				></iframe>
+			</noscript>
+		</html>
+	)
 }
