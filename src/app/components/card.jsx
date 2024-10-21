20806602794e23eda2bdae1a5e438764178162fc
edisson89 <edisson.12@hotmail.com>
2024-10-21 10:55:19 -0500

    ajustes title

--- src/app/components/card.jsx
+++ src/app/components/card.jsx
@@ -5,9 +5,9 @@ import React, { useCallback, useEffect, useState } from "react"
 
 // eslint-disable-next-line react/prop-types
 const Card = ({ title, image, url, precio, oferta }) => {
-	const [oculto, setOculto] = useState(false)
+	const [oculto, setOculto] = useState(true)
 	const [activeIndex, setActiveIndex] = useState(0)
-	const [showTitle, setShowTitle] = useState(true) // Estado para controlar la visibilidad del título
+	const [showTitle, setShowTitle] = useState(false) // Estado para controlar la visibilidad del título
 	
 	
 	const nextSlide = useCallback(() => {
@@ -21,9 +21,9 @@ const Card = ({ title, image, url, precio, oferta }) => {
 	}
 	useEffect(() => {
 		
-		const timer = setInterval(nextSlide, 2200) // Cambia la imagen cada 3 segundos
+		const timer = setInterval(nextSlide, 2600) // Cambia la imagen cada intervalo segundos
 		// Función para alternar la visibilidad del título
-
+		
 		return () => clearInterval(timer) // Limpia el temporizador al desmontar el componente
 	}, [activeIndex, nextSlide]) // Dependencia para reiniciar el temporizador si el activeIndex cambia
 	// Función para alternar la visibilidad del título
@@ -44,7 +44,7 @@ const Card = ({ title, image, url, precio, oferta }) => {
 						<br></br>
 						<br></br>
 
-						<h3 className={styles.precio}>{precio}</h3>
+						<h3 className={styles.precio}>${precio}</h3>
 						<br></br>
 						<h2
 							className={styles.comprar}
@@ -73,7 +73,7 @@ const Card = ({ title, image, url, precio, oferta }) => {
 								className={`${styles.btn}  ${!oculto ? styles.blink : null}`}
 								onClick={toggleTitleVisibility}
 							>
-								{`${!oculto ? 'Ocultar titulo' : 'Mostrar titulo'}`}
+								{`${!oculto ? 'Ocultar Caracteristicas' : 'Caracteristicas'}`}
 							</button>
 							<button
 								className={styles.btn}
